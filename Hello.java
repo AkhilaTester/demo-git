8d5397b32d0d8b697d818fbf40882dae516b0fd2
AkhilaTester <98333655+AkhilaTester@users.noreply.github.com>
2022-01-26 21:56:37 +0530

    updated

--- Hello.java
+++ Hello.java
@@ -1,14 +1,9 @@
 class Hello
 {
 	
-	public static void main(int args[])
-{
-System.out.println("Hello world");
-}
 public static void main(String args[])
 {
-	//int args[]=new int[3];
-//System.out.println("Hello");
-Hello.main(new int [] {1,2,3});
+	
+System.out.println("Hello world");
 }
 }
