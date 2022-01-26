8f78af83574ba6a38fbf16a1e1ed6c29a31c3c2a
unknown <akhilamathew8@gmail.com>
2022-01-26 21:42:41 +0530

    Need to upload hello.java, my  first java file to repository

---
+++ Hello.java
@@ -0,0 +1,14 @@
+class Hello
+{
+	
+	public static void main(int args[])
+{
+System.out.println("Hello world");
+}
+public static void main(String args[])
+{
+	//int args[]=new int[3];
+//System.out.println("Hello");
+Hello.main(new int [] {1,2,3});
+}
+}
